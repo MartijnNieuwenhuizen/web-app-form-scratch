32f0148e9e7efc8fe9f36631b07172c4c1e9be20
NieuweM <martijn.nieuwenhuizen@hva.nl>
2016-02-03 13:54:43 +0100

    made the connections

--- opdracht5/src/js/main.js
+++ opdracht5/src/js/main.js
@@ -1,6 +1,34 @@
 // Add the iifi
 (function() {
+	'use strict';
 
-	// Code
+	var app = {
+		init: function() {
+
+			// call the routes.init
+			routes.init();
+
+		}
+	}
+
+	var routes = {
+		init: function() {
+
+			// haschanged event listener
+			window.addEventListener('hashchange', sections.toggle ,false);
+
+		}
+	}
+
+	var sections = {
+		toggle: function(route) {
+
+			// Add the toggle function: show one / hide the others
+
+		}
+	}
+
+	// start the main app
+	app.init();
 
 })();
