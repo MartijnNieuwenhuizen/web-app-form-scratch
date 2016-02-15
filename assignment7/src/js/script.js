39b6ab8ffacf3f294c50440d043b7c6c4324be09
NieuweM <martijn.nieuwenhuizen@hva.nl>
2016-02-15 16:54:32 +0100

    added real search

--- assignment7/src/js/script.js
+++ assignment7/src/js/script.js
@@ -29,15 +29,18 @@
 					routie('search');
 				},
 				'search': function() {
-			    	var title = "Search";
-			    	soundCloud.action(title);
+			    	// template.render(null, htmlElements.searchform.innerHTML);
+			    	search.action();
 			    },
 				'all': function() {
-			    	var title = "All songs";
-			    	soundCloud.action(title);
+			    	soundCloud.action();
 			    },
 			    '*': function() {
-			    	template.render(null, htmlElements.error.innerHTML);
+			    	var content = {
+			    		title: "404",
+			    		message: "Sorry page not found"
+			    	}
+			    	template.render(content, htmlElements.error.innerHTML);
 			    }
 			});
 
@@ -45,18 +48,6 @@
 	}
 
 	var soundCloud = {
-		url: function(title) {
-
-			var sc = {
-				BaseUrl: "https://api.soundcloud.com",
-				tracks: "tracks?client_id=2fda30f3c5a939525422f47c385564ae",
-				users: "users?client_id=2fda30f3c5a939525422f47c385564ae"
-			}
-			var soundCloudUrl = sc.BaseUrl + '/' + sc.tracks;
-			console.log(soundCloudUrl);
-			return soundCloudUrl;
-
-		},
 		getData: function(url) {
 
 			return new Promise(function(resolve, reject) { // Resolve = .then && Reject = .catch;
@@ -66,20 +57,16 @@
 				request.onloadstart = function() {
 
 					console.log("Start Loading Data");
+					// if more than 250ms
 					spinner.start();
 
 				}
 				request.onloadend = function(response) {
 
-					setTimeout(function(){ 
-
-						console.log("Loaded Data");
-						spinner.stop();
-						var data = request.response;
-						
-						resolve(data);
-						
-					}, 3000);
+					console.log("Loaded Data");
+					var data = request.response;
+					
+					resolve(data);					
 
 				}
 
@@ -91,18 +78,47 @@
 			});
 
 		},
-		action: function(title) {
 
-			this.getData('https://api.soundcloud.com/tracks?client_id=2fda30f3c5a939525422f47c385564ae')
-				// Resolve
+		action: function(term) {
+
+			var sc = {
+				BaseUrl: "https://api.soundcloud.com",
+				id: 'client_id=2fda30f3c5a939525422f47c385564ae',
+				tracks: "tracks?",
+				users: "users?client_id=2fda30f3c5a939525422f47c385564ae",
+				limit: 'limit=200'
+			}
+
+			if ( term ) {
+
+				var soundCloudUrl = sc.BaseUrl + '/' + sc.tracks + sc.id + '&q=' + '=' + term + '&' + sc.limit;				
+
+			} else {
+
+				var soundCloudUrl = sc.BaseUrl + '/' + sc.tracks + sc.id + '&' + sc.limit;
+
+			}
+
+			this.getData(soundCloudUrl)	
 				.then(function(response) {
 
 					console.log("load content");
 					var rawData = JSON.parse(response);
-					template.render(rawData, htmlElements.songs.innerHTML);
+
+					if ( rawData.length ) {
+
+						template.render(rawData, htmlElements.songs.innerHTML);
+
+					} else {
+						
+						var content = {
+							title: "Sorry, no Search results"
+						}
+						template.render(content, htmlElements.error.innerHTML);
+
+					}
 
 				})
-				
 				// Reject
 				.catch(function(err) {
 
@@ -127,14 +143,7 @@
 
 			this.detail();
 
-		},
-		// ToDo: Make this one function
-		renderForm: function(rawData) {
-
-			// render the searchform template
-			this.render(rawData, htmlElements.searchform.innerHTML);
-			// call the search
-			search.action(rawData);
+			spinner.stop();
 
 		},
 		detail: function() {
@@ -161,61 +170,30 @@
 			}
 
 		}
-	}
+	};
 
 	var search = {
-		action: function(rawData) {
+		action: function() {
+
+			template.render(null, htmlElements.searchform.innerHTML);
 
 			var submitButton = document.querySelector('#submit');
 			submitButton.onclick = function() {
 			
-				var searchValue = document.querySelector('#song').value;
-
-				var matchingData = [];
-
-				_.filter(rawData, function(matched) {
-
-					// if the title or genre of a object matches
-				    if( matched.title && matched.title.match(searchValue) || matched.genre && matched.genre.match(searchValue)) {
-
-				    	// push this data in the matchingdata array
-						matchingData.push(matched);
-
-				    }
-				   
-				});   
-
-				// if there are search results
-				if( matchingData.length ) {
-
-					template.render(matchingData, htmlElements.songs.innerHTML);	
-
-				}
-				// if there are no search results
-				else {
-
-					var message = {
-						title: "no search results match"
-					}
-				
-					template.render(message, htmlElements.noSearchResults.innerHTML);
-
-				}
+				var searchValue = document.querySelector('#song').value.toLowerCase();
+				soundCloud.action(searchValue);
 
 			}
 
 		}
 
-	}
+	};
 
 	var spinner = {
 		start: function() {
 
 			console.log("Start Spinner");
-			var message = {
-				title: "no search results match"
-			}
-			template.render(message, htmlElements.loading.innerHTML);
+			template.render(null, htmlElements.loading.innerHTML);
 
 		},
 		stop: function() {
@@ -223,7 +201,7 @@
 			console.log("Stop Spinner");
 
 		}
-	}
+	};
 
 	// start the main app
 	app.init();
